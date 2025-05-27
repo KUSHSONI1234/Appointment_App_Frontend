d62f2768bcbe2e21c13035dae7a98aed03e7e997
unknown <User@600000-96.ad.lan>
2025-05-27 11:33:36 +0530

    changes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,8 +9,8 @@ import {
   HttpErrorResponse,
   HttpClientModule,
 } from '@angular/common/http';
-// import { environment } from '../../../environments/environment.prod';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
+// import { environment } from '../../../environments/environment';
 
 
 @Component({
