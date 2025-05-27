23aee284efe34340d5e8531839fa4719cd86ba16
unknown <User@600000-96.ad.lan>
2025-05-27 11:07:29 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
+// src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl:'https://appointment-app-backend-p24n.onrender.com'
-
+  apiUrl: 'https://appointment-app-backend-p24n.onrender.com/api'  // ✅ use deployed backend here
 };
